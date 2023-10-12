d8562c8ebb8f5ba19e43c7057c20977879f0d681
sarveshshahi <sarveshshahi1210@gmail.com>
2023-10-12 15:09:00 +0530

    01_variable.js

---
+++ 02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3+3)
+console.log("Hitesh")
+
+let name= "sarvesh"
+let age = 18
+let isLoggedIN = false
+
+//number =>2 to power 53
+//bigint
+//string==> true/false
+//null => standalone value // empty (not zero 0)
+//undefiend=>
+// symbole (use to identifiy 'Uniqeness'(figma tool use this symbole);
+
+//object
+
+
+
+console.log("type of variable : ", typeof age); //object
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); // undefined:
+
+//learnt ecma script specification
