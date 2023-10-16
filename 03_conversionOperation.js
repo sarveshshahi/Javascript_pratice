f14a72b0bf1f5bed176b73cde0b72462c8eb40e6
sarveshshahi <sarveshshahi1210@gmail.com>
2023-10-16 12:36:35 +0530

    conparision

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,1 +1,28 @@
+let score = true
+//
+//console.log(typeof score);
+//console.log(typeof (score));
+//
+let valueINumber = Number(score) // it take a integer in a number from.
+//console.log(typeof valueINumber);
+//console.log(valueINumber); // NaN means not a number(it ouccer when it is not conver in number
+//// "33"=>33
+////"33abc" => NaN
+//// true =>1; false =>0;
 
+let isLoggedIn = 1
+
+let booleanIsLoggedIn= boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>
+
+//
+let someNumber =33
+let stringNumber=String(someNumber)
+//********************************Operation************************
+
+let value=3
+let negValue=-value
+console.log(negValue)
